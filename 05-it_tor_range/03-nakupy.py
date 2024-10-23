9176624d8d0dac24d9a72bcf6d1d97bb4f9f6160
rabikmatias@gmail,com <rabikmatias@gmail.com>
2024-10-23 10:04:22 +0200

    add 05

---
+++ 05-it_tor_range/03-nakupy.py
@@ -0,0 +1,8 @@
+pocet = 0
+celková_suma = 0
+for suma in 1.5, 10.20, 25.3, 4, 5.50:
+    pocet = pocet + 1
+    celková_suma - celková_suma + suma
+    print(f"Suma {pocet}. nákupu: {suma}€")
+print(f"Celkový počet nákupov: {pocet}")
+print(f"Celková suma: {celková_suma}")
