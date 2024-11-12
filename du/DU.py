33fdb73865710601e52d8278a9c7945fea8116c4
Matias Rabik <rabikmatias@gmail.com>
2024-11-12 22:35:49 +0100

    add

---
+++ du/DU.py
@@ -0,0 +1,64 @@
+import tkinter
+
+canavas = tkinter.Canvas(width = 1000, height = 1000)
+canavas.pack()
+
+canavas.create_rectangle(100, 50, 50, 100, fill="red")
+
+canavas.create_rectangle(150, 100, 100, 150, fill="red")
+canavas.create_rectangle(250, 100, 200, 150, fill="red")
+canavas.create_rectangle(300, 50, 250, 100, fill="red")
+
+canavas.create_rectangle(100, 100, 50, 300, fill="red")
+canavas.create_rectangle(100, 150, 50, 300, fill="red")
+canavas.create_rectangle(100, 200, 50, 300, fill="red")
+canavas.create_rectangle(100, 250, 50, 300, fill="red")
+canavas.create_rectangle(100, 300, 50, 300, fill="red")
+canavas.create_rectangle(100, 350, 50, 300, fill="red")
+canavas.create_rectangle(100, 400, 50, 300, fill="red")
+
+canavas.create_rectangle(300, 150, 250, 100, fill="red")
+canavas.create_rectangle(300, 200, 250, 100, fill="red")
+canavas.create_rectangle(300, 250, 250, 100, fill="red")
+canavas.create_rectangle(300, 300, 250, 100, fill="red")
+canavas.create_rectangle(300, 350, 250, 100, fill="red")
+canavas.create_rectangle(300, 400, 250, 100, fill="red")
+
+canavas.create_rectangle(200, 150, 150, 200, fill="red")
+canavas.create_rectangle(200, 250, 150, 200, fill="red")
+
+canavas.create_rectangle(400, 50, 350, 100, fill="red")
+canavas.create_rectangle(450, 50, 400, 100, fill="red")
+canavas.create_rectangle(500, 50, 450, 100, fill="red")
+canavas.create_rectangle(550, 50, 500, 100, fill="red")
+canavas.create_rectangle(400, 50, 500, 100, fill="red")
+canavas.create_rectangle(600, 150, 550, 100, fill="red")
+canavas.create_rectangle(400, 150, 350, 100, fill="red")
+canavas.create_rectangle(600, 200, 550, 100, fill="red")
+canavas.create_rectangle(400, 200, 350, 100, fill="red")
+canavas.create_rectangle(450, 250, 350, 200, fill="red")
+canavas.create_rectangle(500, 250, 400, 200, fill="red")
+canavas.create_rectangle(550, 250, 400, 200, fill="red")
+canavas.create_rectangle(400, 250, 350, 100, fill="red")
+canavas.create_rectangle(500, 300, 450, 250, fill="red")
+canavas.create_rectangle(400, 300, 350, 100, fill="red")
+canavas.create_rectangle(550, 350, 500, 300, fill="red")
+canavas.create_rectangle(400, 350, 350, 100, fill="red")
+canavas.create_rectangle(600, 400, 550, 350, fill="red")
+canavas.create_rectangle(400, 400, 350, 100, fill="red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+tkinter.mainloop() 
+
+
---
+++ du/Dú.py
@@ -0,0 +1,56 @@
+import tkinter
+
+canavas = tkinter.Canvas(width = 1000, height = 1000)
+canavas.pack()
+
+canavas.create_rectangle(10, 10, 20, 50, fill="red")
+canavas.create_rectangle(10, 20, 20, 50, fill="red")
+canavas.create_rectangle(10, 30, 20, 50, fill="red")
+canavas.create_rectangle(10, 40, 20, 50, fill="red")
+canavas.create_rectangle(10, 50, 20, 50, fill="red")
+canavas.create_rectangle(10, 60, 20, 50, fill="red")
+canavas.create_rectangle(10, 70, 20, 60, fill="red")
+canavas.create_rectangle(10, 80, 20, 60, fill="red")
+
+canavas.create_rectangle(60, 20, 50, 20, fill="red")
+canavas.create_rectangle(60, 30, 50, 70, fill="red")
+canavas.create_rectangle(60, 40, 50, 10, fill="red")
+canavas.create_rectangle(60, 50, 50, 40, fill="red")
+canavas.create_rectangle(60, 60, 50, 30, fill="red")
+canavas.create_rectangle(60, 70, 50, 30, fill="red")
+canavas.create_rectangle(60, 80, 50, 30, fill="red")
+
+canavas.create_rectangle(20, 30, 30, 20, fill="red")
+canavas.create_rectangle(40, 30, 50, 20, fill="red")
+
+canavas.create_rectangle(30, 40, 40, 30, fill="red")
+canavas.create_rectangle(30, 50, 40, 30, fill="red")
+
+
+canavas.create_rectangle(80, 10, 70, 20, fill="red")
+canavas.create_rectangle(80, 20, 70, 20, fill="red")
+canavas.create_rectangle(80, 30, 70, 20, fill="red")
+canavas.create_rectangle(80, 40, 70, 20, fill="red")
+canavas.create_rectangle(80, 50, 70, 20, fill="red")
+canavas.create_rectangle(80, 60, 70, 20, fill="red")
+canavas.create_rectangle(80, 70, 70, 20, fill="red")
+canavas.create_rectangle(80, 80, 70, 20, fill="red")
+
+canavas.create_rectangle(90, 10, 80, 20, fill="red")
+canavas.create_rectangle(100, 10, 90, 20, fill="red")
+canavas.create_rectangle(110, 10, 100, 20, fill="red")
+
+canavas.create_rectangle(120, 30, 110, 20, fill="red")
+canavas.create_rectangle(120, 40, 110, 20, fill="red")
+
+canavas.create_rectangle(90, 50, 70, 40, fill="red")
+canavas.create_rectangle(100, 50, 80, 40, fill="red")
+canavas.create_rectangle(110, 50, 80, 40, fill="red")
+
+canavas.create_rectangle(100, 60, 90, 50, fill="red")
+canavas.create_rectangle(110, 70, 100, 60, fill="red")
+canavas.create_rectangle(120, 80, 110, 70, fill="red")
+
+
+
+tkinter.mainloop()
