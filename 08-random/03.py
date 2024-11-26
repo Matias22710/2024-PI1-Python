9b41c7c8bfd20eb91d79313740f52cfe16746057
Matias Rabik <rabikmatias@gmail.com>
2024-11-26 20:34:51 +0100

    add

--- 08-random/03.py
+++ 08-random/03.py
@@ -1,22 +1,23 @@
-import tkinter
-import random as tk
+import tkinter , random
 
-pocet = int(input("Zadaj počet jednotiek: "))
+pocet = int(input("Zadaj počet jednotiek:\n"))
 
-canvas = tkinter.Canvas(width=500, height=400)
+canvas = tkinter.Canvas(width=1000, height=1000)
 canvas.pack()
 
-def jednotka(x, y):
-    canvas.create_rectangle(x, y+10, x+10, y+20, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y, x+20, y+10, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+10, x+20, y+20, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+20, x+20, y+30, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+30, x+20, y+40, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+40, x+20, y+50, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+50, x+20, y+60, fill=random.choice(["red", "blue", "green"]))
-    canvas.create_rectangle(x+10, y+60, x+20, y+70, fill=random.choice(["red", "blue", "green"]))
-
-
+for i in range(pocet):
+    x = random.randint(3, 1000-30-3)
+    y = random.randint(3, 1000-70-3)
+    canvas.create_rectangle(x, y+10, x+10, y+20, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y, x+20, y+10, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+10, x+20, y+20, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+20, x+20, y+30, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+30, x+20, y+40, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+40, x+20, y+50, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+50, x+20, y+60, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+60, x+20, y+70, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x, y+60, x+10, y+70, fill=random.choices(["red", "blue", "green"]))
+    canvas.create_rectangle(x+10, y+60, x+20, y+70, fill=random.choices(["red", "blue", "green"]))
 
 
 canvas.mainloop()
